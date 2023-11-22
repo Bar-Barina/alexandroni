281d27d3f0047d7a4bf8436103c69f2d474b9bdd
Bar Barina <itsbarbarina@gmail.com>
2023-11-22 17:39:33 +0200

    added side headline

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import '../src/assets/styles/styles.scss'
+import SideHeadline from './cmps/sideHeadline';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <SideHeadline/>
     </div>
   );
 }
---
+++ src/cmps/sideHeadline.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function sideHeadline() {
+  return (
+    <section className='sideHeadline-container'>
+        <h1 className='flex auto-center'>אלכסנדרוני</h1>
+        <img src="https://www.alexandroni.org/wp-content/uploads/2017/06/badge2.png" alt="" />
+        <div className='sec-title flex column auto-center'>
+        <h5>מלחמת חרבות ברזל</h5>
+        <h5>2023</h5>
+        </div>
+    </section>
+  )
+}
